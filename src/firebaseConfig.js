3f3d50ff2374587ac3bd4df965d745183af93d95
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2025-08-03 12:54:03 -0400

    connecting to github

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOHsPjvetkqeebVXgTYQiWGJEfD4Dn84g",
+  authDomain: "wedding-site-40d2b.firebaseapp.com",
+  projectId: "wedding-site-40d2b",
+  storageBucket: "wedding-site-40d2b.firebasestorage.app",
+  messagingSenderId: "934255129860",
+  appId: "1:934255129860:web:919d84c993e47aeeedcc51",
+};
+
+const app = initializeApp(firebaseConfig);
